2b58db780a0788898ab3e8eeff7eeb8c3e1c6966
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2022-12-31 14:09:29 +0900

    [add] update index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { HashPair } from './cores/HashPair';
 export { Contracts } from './cores/Contracts';
 export * from './servicies/HTLCService';
 export * from './servicies/HTLCERC20Service';
 export * from './servicies/HTLCERC721Service';
 export * from './servicies/HTLCSymbolService';
+export * from './models';
